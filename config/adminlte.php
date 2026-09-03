18789b997cd232109adf1e4ca7c188b44c2b1513
Tatan <tatangamess@gmail.com>
2026-09-03 14:41:39 -0600

    cambios

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'Proveeduria',
+    'title' => 'Mercados',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Proveeduria</b>',
+    'logo' => '<b>Mercados</b>',
     'logo_img' => 'images/logosistema.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
--- resources/views/frontend/login/vistalogin.blade.php
+++ resources/views/frontend/login/vistalogin.blade.php
@@ -4,7 +4,7 @@
 @include('backend.urlglobal')
 
 <head>
-    <title>Proveeduria</title>
+    <title>Mercados</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/login/bootstrap.min.css') }}">
@@ -117,7 +117,7 @@
 
 
                         <!-- TÍTULO  -->
-                        <h4 class="login-title">PROVEEDURIA Y BODEGA</h4>
+                        <h4 class="login-title">MERCADOS</h4>
                         <form class="login-form">
                             <div>
                                 <label class="font-500">Usuario</label>
